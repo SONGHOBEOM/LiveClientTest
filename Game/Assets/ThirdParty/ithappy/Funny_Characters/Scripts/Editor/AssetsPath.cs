2d1edd8b0eb056776203642c14c3ccdcfd2684bc
Hobeom <hb.song@oddonegames.com>
2025-03-21 02:48:04 +0900

    캐릭터 에셋 임포트

---
+++ Game/Assets/ThirdParty/ithappy/Funny_Characters/Scripts/Editor/AssetsPath.cs
@@ -0,0 +1,16 @@
+﻿namespace CharacterCustomization
+{
+    public static class AssetsPath
+    {
+        public const string MainMaterial = Root + "Materials/Material.mat";
+        public const string GlassMaterial = Root + "Materials/Glass.mat";
+        public const string EmissionMaterial = Root + "Materials/Emission.mat";
+        public const string Parts = Root + "Meshes";
+        public const string FullBody = Root + "Meshes/Full_Body";
+        public const string Fbx = Root + "Meshes/Basic_Character.fbx";
+        public const string AnimationController = Root + "Animations/AnimationController.controller";
+        public const string SavedCharacters = Root + "Saved_Characters/";
+
+        private const string Root = "Assets/ThirdParty/ithappy/Funny_Characters/";
+    }
+}
